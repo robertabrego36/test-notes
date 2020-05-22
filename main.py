738537cce6e144a2bc0625092f2c4212dc5175fa
robertabrego36 <62353193+robertabrego36@users.noreply.github.com>
2020-05-22 09:50:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-BTC_API_KEY = 't211511pw8xqpqy1nf5egojn0d5qy7esf6qplnboxk25mzmxge51fo5r92wd46kq'
+secret = 't211511pw8xqpqy1nf5egojn0d5qy7esf6qplnboxk25mzmxge51fo5r92wd46kq'
 
-BINANCE API KEY = 'kkba3gl9o2b265gfy3c7nkfg9dkiuzt1v67r2n1w704rrxcyfd2pvqrvooyjhdue'
+BINANCE_API_KEY = 'kkba3gl9o2b265gfy3c7nkfg9dkiuzt1v67r2n1w704rrxcyfd2pvqrvooyjhdue'
+
+def main():
+  print 'test'
